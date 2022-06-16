0d949c2b083545ce193ffe79988a65dff017be1a
john0123456789 <johnluongpham@yahoo.com>
2022-06-16 10:56:46 -0700

    added dynamic consumer

---
+++ src/components/Match.js
@@ -0,0 +1,16 @@
+import { useState, useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
+
+const Match = () => {
+  const [match, setMatch] = useState(false);
+  const { sign } = useContext(HoroscopeContext);
+  console.log(sign);
+  return (
+    <>
+      <button onClick={() => setMatch(!match)}>Match</button>
+      {match ? <div>{sign.match}</div> : null}
+    </>
+  );
+};
+
+export default Match;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
-import horoscopesObj from '../data/horoscopes';
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
+import horoscopesObj from "../data/horoscopes";
 
 const Navbar = () => {
+  console.log("Navbar Rendering!");
   const horoscopes = Object.keys(horoscopesObj);
+  const { setCurrentSign } = useContext(HoroscopeContext);
 
   return (
     <nav>
-      {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+      {horoscopes.map((sign) => (
+        <span onClick={() => setCurrentSign(sign)} key={sign}>
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
